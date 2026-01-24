816e487e2b0d71d9eca32d68a69eed4de72bff88
bthomas <brthomasusa@gmail.com>
2026-01-24 13:44:06 -0600

    CompanyEditPage markup is complete

--- CloudAccounting.Wasm/Pages/Company/CompanyEditPage.razor.cs
+++ CloudAccounting.Wasm/Pages/Company/CompanyEditPage.razor.cs
@@ -1,7 +1,58 @@
-﻿namespace CloudAccounting.Wasm.Pages.Company
+﻿using CloudAccounting.Wasm.Services.Repositories.Company;
+using Radzen.Blazor;
+using System.Collections.ObjectModel;
+
+namespace CloudAccounting.Wasm.Pages.Company
 {
     public partial class CompanyEditPage
     {
         [Parameter] public int CompanyCode { get; set; }
+        [Inject] private ICompanyService? CompanyService { get; set; }
+        [Inject] private NotificationService? NotificationService { get; set; }
+        [Inject] private NavigationManager? Navigation { get; set; }
+        [Inject] private ILogger<CompanyEditPage>? Logger { get; set; }
+
+        private CompanyDetail? _company;
+        private string? _companyName;
+        private static ReadOnlyCollection<string> _currencies = ["CAN", "USD"];
+        private RadzenTemplateForm<CompanyDetail>? _companyDetailForm;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            Result<CompanyDetail> result = await CompanyService!.GetCompanyByIdAsync(CompanyCode);
+
+            if (result.IsSuccess)
+            {
+                _company = result.Value;
+                _companyName = result.Value.CompanyName;
+            }
+            else 
+            {
+                Logger!.LogError("Failed to retrieve company: {ERROR}.", result.Error.Message);
+
+                ShowErrorNotification.ShowError(
+                    NotificationService!,
+                    result.Error.Message
+                );
+
+                Navigation?.NavigateTo("/Pages/Company/CompaniesListPage");
+            }
+        }
+
+        private void Submit(CompanyDetail arg)
+        {
+            // manual validation
+            // bool isValid = _companyDetailForm.EditContext.Validate();
+        }
+
+        private void Cancel()
+        {
+            Navigation?.NavigateTo("/Pages/Company/CompaniesListPage");
+        }
+
+        private void Delete()
+        {
+            Navigation?.NavigateTo("/Pages/Company/CompaniesListPage");
+        }
     }
 }
