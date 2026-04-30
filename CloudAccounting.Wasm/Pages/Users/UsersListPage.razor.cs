c9296cac12040c4c45bf20d2c2ded15652ed819b
bthomas <brthomasusa@gmail.com>
2026-04-30 11:30:12 -0500

    Added security to Settings submenus

---
+++ CloudAccounting.Wasm/Pages/Users/UsersListPage.razor.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudAccounting.Wasm.Pages.Users
+{
+    public partial class UsersListPage
+    {
+        
+    }
+}
